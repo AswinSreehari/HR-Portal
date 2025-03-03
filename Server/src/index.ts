61f03ea55c14360d7af8e58fe800492be9ea13fa
Aswin Sreehari <aswinchittethu@gmail.com>
2025-03-03 15:35:40 +0530

    refactor

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -15,7 +15,7 @@ const app: Express = express();
 // const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: "https://hr-portal-client-phi.vercel.app"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
